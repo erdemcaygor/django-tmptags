f9978b2a27615e15a2d5f560c3cf36457409c270
erdemcaygor <erdem.caygor@gmail.com>
2015-12-21 08:36:28 +0200

    Update models.py

--- tmptags/models.py
+++ tmptags/models.py
@@ -1,17 +1,1 @@
-# -*- coding: utf-8 -*-
-from django.db import models
-from django.contrib.auth.models import User
 
-class Deneme(models.Model):
-
-    is_active = models.BooleanField(default=False, verbose_name='Aktif mi?')
-    name = models.CharField(max_length=50, verbose_name='İsim')
-    is_deleted = models.BooleanField(default=False, verbose_name='Silindi Mi?')
-    salary = models.IntegerField(verbose_name='Gelir')
-    user = models.ForeignKey(User)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name_plural = 'Deneme'
