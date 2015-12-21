4a8ae5e1db230198e183219e589b8f37a67f48a0
erdemcaygor <erdem.caygor@gmail.com>
2015-12-21 08:39:00 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+
+
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+setup(
+    name='django-tmptags',
+    version='0.1',
+    packages=['tmptags'],
+    include_package_data=True,
+    license='BSD License',
+    description='A simple Django app to query filter, check permission and group control in template.',
+    long_description=README,
+    url='',
+    author='Erdem Çaygör',
+    author_email='erdem.caygor@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
